536348978fa63c276d9ede4e5eca597ced6ffd85
andrehsvictor <andrehsvictor@gmail.com>
2025-05-30 16:41:10 -0300

    feat: add RabbitMQ configuration with image deletion queue and RabbitAdmin setup

---
+++ src/main/java/andrehsvictor/mooral/rabbitmq/RabbitMqConfig.java
@@ -0,0 +1,25 @@
+package andrehsvictor.mooral.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    Queue imageDeletionQueue() {
+        return new Queue("images.v1.deletion", true, false, false);
+    }
+
+    @Bean
+    RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        rabbitAdmin.declareQueue(imageDeletionQueue());
+        return rabbitAdmin;
+    }
+
+}
