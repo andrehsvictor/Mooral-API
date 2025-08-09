e3af00938bd208b4e0f9b7cd1baeb2495d0acab8
André Victor <andrehsvictor@gmail.com>
2025-08-09 15:18:00 -0300

    feat: add methods to retrieve current user ID and permissions in JwtService

--- src/main/java/io/github/andrehsvictor/mooral_api/common/jwt/JwtService.java
+++ src/main/java/io/github/andrehsvictor/mooral_api/common/jwt/JwtService.java
@@ -1,11 +1,15 @@
 package io.github.andrehsvictor.mooral_api.common.jwt;
 
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,7 @@ public class JwtService {
     private final JwtEncoder jwtEncoder;
     private final JwtDecoder jwtDecoder;
     private final JwtProperties jwtProperties;
+    private final SecurityContextHolderStrategy securityContextHolderStrategy;
 
     public Jwt encodeAccessToken(Authentication authentication, String sessionId) {
         return createTokenBuilder(authentication)
@@ -44,5 +49,25 @@ public class JwtService {
 
     public Jwt decode(String token) {
         return jwtDecoder.decode(token);
+    }
+
+    public UUID getCurrentUserId() {
+        Authentication authentication = securityContextHolderStrategy.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof JwtAuthenticationToken jwtAuth) {
+            Jwt jwt = jwtAuth.getToken();
+            String sub = jwt.getSubject();
+            return UUID.fromString(sub);
+        }
+        return null;
+    }
+
+    public List<String> getCurrentUserPermissions() {
+        Authentication authentication = securityContextHolderStrategy.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof JwtAuthenticationToken jwtAuth) {
+            Jwt jwt = jwtAuth.getToken();
+            String scope = jwt.getClaimAsString(JwtClaim.SCOPE.getValue());
+            return List.of(scope.split(" "));
+        }
+        return List.of();
     }
 }
