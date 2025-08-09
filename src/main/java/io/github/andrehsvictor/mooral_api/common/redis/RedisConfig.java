4413a47f329b0ffd980038bb2e7f10bc5818bdcf
André Victor <andrehsvictor@gmail.com>
2025-08-09 14:45:37 -0300

    feat: add RedisConfig for Redis template configuration

---
+++ src/main/java/io/github/andrehsvictor/mooral_api/common/redis/RedisConfig.java
@@ -0,0 +1,22 @@
+package io.github.andrehsvictor.mooral_api.common.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    <T> RedisTemplate<String, T> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, T> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+}
