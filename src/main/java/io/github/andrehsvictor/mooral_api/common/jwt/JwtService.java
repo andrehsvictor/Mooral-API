4336b2197c67fced46346d21b984c8b034e9cf03
André Victor <andrehsvictor@gmail.com>
2025-08-09 14:12:39 -0300

    refactor: streamline JWT token encoding methods and enhance claim handling

--- src/main/java/io/github/andrehsvictor/mooral_api/common/jwt/JwtService.java
+++ src/main/java/io/github/andrehsvictor/mooral_api/common/jwt/JwtService.java
@@ -1,14 +1,15 @@
 package io.github.andrehsvictor.mooral_api.common.jwt;
 
+import java.time.Duration;
 import java.time.Instant;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -20,55 +21,49 @@ import lombok.RequiredArgsConstructor;
 public class JwtService {
 
     private final JwtEncoder jwtEncoder;
-    private final JwtDecoder jwtDecoder;
     private final JwtProperties jwtProperties;
 
     public Jwt encodeAccessToken(Authentication authentication, String sessionId) {
-        Instant iat = Instant.now();
-        Instant exp = iat.plus(jwtProperties.getAccessTokenLifespan());
-        JwtClaimsSet.Builder claims = JwtClaimsSet.builder()
-                .issuer(jwtProperties.getIssuer())
-                .audience(jwtProperties.getAudiences())
-                .subject(authentication.getName())
-                .issuedAt(iat)
-                .expiresAt(exp)
-                .claim(JwtClaim.SESSION_ID.value, sessionId)
-                .claim(JwtClaim.SCOPE.value, String.join(" ", authentication.getAuthorities()))
-                .claim(JwtClaim.TYPE.value, JwtType.BEARER.value);
-
-        return jwtEncoder.encode(JwtEncoderParameters.from(claims.build()));
+        Map<String, Object> extraClaims = Map.of(
+            JwtClaim.SESSION_ID.value, sessionId,
+            JwtClaim.SCOPE.value, getScope(authentication)
+        );
+        return createToken(authentication, JwtType.BEARER, jwtProperties.getAccessTokenLifespan(), extraClaims);
     }
 
     public Jwt encodeRefreshToken(Authentication authentication, String sessionId) {
-        Instant iat = Instant.now();
-        Instant exp = iat.plus(jwtProperties.getRefreshTokenLifespan());
-        JwtClaimsSet.Builder claims = JwtClaimsSet.builder()
-                .issuer(jwtProperties.getIssuer())
-                .audience(jwtProperties.getAudiences())
-                .subject(authentication.getName())
-                .issuedAt(iat)
-                .expiresAt(exp)
-                .claim(JwtClaim.SESSION_ID.value, sessionId)
-                .claim(JwtClaim.SCOPE.value, String.join(" ", authentication.getAuthorities()))
-                .claim(JwtClaim.TYPE.value, JwtType.REFRESH.value);
-
-        return jwtEncoder.encode(JwtEncoderParameters.from(claims.build()));
+        Map<String, Object> extraClaims = Map.of(
+            JwtClaim.SESSION_ID.value, sessionId,
+            JwtClaim.SCOPE.value, getScope(authentication)
+        );
+        return createToken(authentication, JwtType.REFRESH, jwtProperties.getRefreshTokenLifespan(), extraClaims);
     }
 
-    public Jwt encodeActionToken(Authentication authentication, JwtAction action,
-            Map<String, Object> additionalClaims) {
-        Instant iat = Instant.now();
-        Instant exp = iat.plus(jwtProperties.getActionTokenLifespan());
+    public Jwt encodeActionToken(Authentication authentication, JwtAction action, Map<String, Object> additionalClaims) {
+        Map<String, Object> extraClaims = new HashMap<>(additionalClaims);
+        extraClaims.put(JwtClaim.ACTION.value, action.value);
+        return createToken(authentication, JwtType.ACTION, jwtProperties.getActionTokenLifespan(), extraClaims);
+    }
+
+    private String getScope(Authentication authentication) {
+        return authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+    }
+
+    private Jwt createToken(Authentication authentication, JwtType type, Duration lifespan, Map<String, Object> extraClaims) {
+        Instant now = Instant.now();
+        Instant expiration = now.plus(lifespan);
+        
         JwtClaimsSet.Builder claims = JwtClaimsSet.builder()
                 .issuer(jwtProperties.getIssuer())
                 .audience(jwtProperties.getAudiences())
                 .subject(authentication.getName())
-                .issuedAt(iat)
-                .expiresAt(exp)
-                .claim(JwtClaim.TYPE.value, JwtType.ACTION.value)
-                .claim(JwtClaim.ACTION.value, action.value);
+                .issuedAt(now)
+                .expiresAt(expiration)
+                .claim(JwtClaim.TYPE.value, type.value);
 
-        additionalClaims.forEach(claims::claim);
+        extraClaims.forEach(claims::claim);
 
         return jwtEncoder.encode(JwtEncoderParameters.from(claims.build()));
     }
