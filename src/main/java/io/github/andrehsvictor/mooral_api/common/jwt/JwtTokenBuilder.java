774978b1f0f829dc6902d5dbefa9fc9e61c69858
André Victor <andrehsvictor@gmail.com>
2025-08-09 14:50:20 -0300

    refactor: clean up whitespace and improve JWT claims handling in JwtTokenBuilder

--- src/main/java/io/github/andrehsvictor/mooral_api/common/jwt/JwtTokenBuilder.java
+++ src/main/java/io/github/andrehsvictor/mooral_api/common/jwt/JwtTokenBuilder.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.Authentication;
@@ -14,21 +15,21 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 
 public class JwtTokenBuilder {
-    
+
     private final JwtEncoder jwtEncoder;
     private final JwtProperties jwtProperties;
     private final Authentication authentication;
     private final Map<String, Object> extraClaims = new HashMap<>();
-    
+
     private JwtType type;
     private Duration lifespan;
-    
+
     public JwtTokenBuilder(JwtEncoder jwtEncoder, JwtProperties jwtProperties, Authentication authentication) {
         this.jwtEncoder = jwtEncoder;
         this.jwtProperties = jwtProperties;
         this.authentication = authentication;
     }
-    
+
     public JwtTokenBuilder asAccessToken(String sessionId) {
         this.type = JwtType.BEARER;
         this.lifespan = jwtProperties.getAccessTokenLifespan();
@@ -36,7 +37,7 @@ public class JwtTokenBuilder {
         this.extraClaims.put(JwtClaim.SCOPE.getValue(), getScope());
         return this;
     }
-    
+
     public JwtTokenBuilder asRefreshToken(String sessionId) {
         this.type = JwtType.REFRESH;
         this.lifespan = jwtProperties.getRefreshTokenLifespan();
@@ -44,29 +45,30 @@ public class JwtTokenBuilder {
         this.extraClaims.put(JwtClaim.SCOPE.getValue(), getScope());
         return this;
     }
-    
+
     public JwtTokenBuilder asActionToken(JwtAction action) {
         this.type = JwtType.ACTION;
         this.lifespan = jwtProperties.getActionTokenLifespan();
         this.extraClaims.put(JwtClaim.ACTION.getValue(), action.getValue());
         return this;
     }
-    
+
     public JwtTokenBuilder withClaim(String key, Object value) {
         this.extraClaims.put(key, value);
         return this;
     }
-    
+
     public JwtTokenBuilder withClaims(Map<String, Object> claims) {
         this.extraClaims.putAll(claims);
         return this;
     }
-    
+
     public Jwt build() {
         Instant now = Instant.now();
         Instant expiration = now.plus(lifespan);
-        
+
         JwtClaimsSet.Builder claims = JwtClaimsSet.builder()
+                .id(UUID.randomUUID().toString())
                 .issuer(jwtProperties.getIssuer())
                 .audience(jwtProperties.getAudiences())
                 .subject(authentication.getName())
@@ -78,7 +80,7 @@ public class JwtTokenBuilder {
 
         return jwtEncoder.encode(JwtEncoderParameters.from(claims.build()));
     }
-    
+
     private String getScope() {
         return authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
