ad06574716f8661453da1ca6ea360e7dc3f0c6df
André Victor <andrehsvictor@gmail.com>
2025-08-09 15:12:53 -0300

    feat: add Session entity for managing user sessions in Redis

---
+++ src/main/java/io/github/andrehsvictor/mooral_api/session/Session.java
@@ -0,0 +1,43 @@
+package io.github.andrehsvictor.mooral_api.session;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash("session")
+@EqualsAndHashCode(of = { "id" })
+public class Session implements Serializable {
+
+    private static final long serialVersionUID = -4933111494402809678L;
+
+    @Id
+    private UUID id;
+
+    @Indexed
+    private UUID jti;
+
+    @Indexed
+    private UUID userId;
+
+    private String ipAddress;
+    private String userAgent;
+    private String device;
+    private Long createdAt;
+    private Long expiresAt;
+
+}
